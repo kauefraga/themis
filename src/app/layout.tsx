de8fb0fc4b8952d172cc3faa9945d5db55fcc2e0
Kauê Fraga Rodrigues <88486000+kauefraga@users.noreply.github.com>
2024-09-07 21:47:26 -0300

    feat: use inter font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Themis',
@@ -13,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
