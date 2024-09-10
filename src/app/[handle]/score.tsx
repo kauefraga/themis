401ea82a4957117e3648b7ac979ed89442b952b7
Kauê Fraga Rodrigues <88486000+kauefraga@users.noreply.github.com>
2024-09-10 20:07:46 -0300

    feat: add new buffs and debuffs, round final score and remove useMemo

--- src/app/[handle]/score.tsx
+++ src/app/[handle]/score.tsx
@@ -1,30 +1,61 @@
 'use client';
 
 import { ProfileViewDetailed } from '@atproto/api/dist/client/types/app/bsky/actor/defs';
-import { useMemo } from 'react';
-
-interface ScoreProps {
-  profile: ProfileViewDetailed;
-}
 
 function calculateProfileScore(profile: ProfileViewDetailed) {
   let score = 0;
 
   if (profile.displayName) {
     // has display name
-    score = 5;
+    score += 5;
 
     if (profile.displayName.length <= 10) {
       // short display name buff
       score *= 2;
     }
+
+    // TODO: display name includes emoji/unicode buff
   }
 
-  return score;
+  if (profile.banner) {
+    // has banner
+    score += 5;
+  }
+
+  if (profile.avatar) {
+    // has avatar
+    score += 5;
+  }
+
+  if (/\d/g.test(profile.handle)) {
+    // handle includes number debuff
+    score -= 5;
+  }
+
+  if (profile.description) {
+    // has description
+    score += 5;
+
+    if (profile.description.length >= 20 && profile.description.length <= 50) {
+      // RELATIVE: informative description buff
+      score *= 2;
+    }
+  }
+
+  if (profile.followersCount && profile.followsCount) {
+    // follow ratio buff
+    score *= profile.followersCount / profile.followsCount;
+  }
+
+  return Math.ceil(score);
+}
+
+interface ScoreProps {
+  profile: ProfileViewDetailed;
 }
 
 export function Score({ profile }: ScoreProps) {
-  const finalScore = useMemo(() => calculateProfileScore(profile), [profile]);
+  const profileScore = calculateProfileScore(profile);
 
-  return <p className="text-xl text-blue-600">Pontuação: {finalScore}</p>;
+  return <p className="text-xl text-blue-600">Pontuação: {profileScore}</p>;
 }
