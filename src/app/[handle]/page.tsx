47af92e1435018d1f1a62a1a8bcdd8c87daaf058
Jefferson Quesado <jeff.quesado@gmail.com>
2024-09-12 22:49:31 -0300

    Testing when profile is not available

--- src/app/[handle]/page.tsx
+++ src/app/[handle]/page.tsx
@@ -10,47 +10,55 @@ type StaticParams = {
 };
 
 export default async function ProfileAnalysis({ params }: StaticParams) {
-  const { data } = await agent.getProfile({
+  const { data, success } = await agent.getProfile({
     actor: params.handle,
   });
 
-  return (
-    <div className="grid min-h-[100dvh] grid-rows-[1fr_auto] justify-items-center">
-      <div className="p-4">
-        <main className="mt-20 flex h-fit w-full max-w-xl flex-col rounded-md border border-black/90 bg-zinc-200">
-          {data.banner && (
-            <Image
-              src={data.banner}
-              width="800"
-              height="300"
-              loading="lazy"
-              alt="Banner do perfil"
-              className="w-full rounded-tl rounded-tr border-b border-black/90"
-            />
-          )}
-          <div className="flex -translate-y-1/4 flex-col gap-3 px-5 py-3">
-            {data.avatar && (
+  if (success) {
+    return (
+      <div className="grid min-h-[100dvh] grid-rows-[1fr_auto] justify-items-center">
+        <div className="p-4">
+          <main className="mt-20 flex h-fit w-full max-w-xl flex-col rounded-md border border-black/90 bg-zinc-200">
+            {data.banner && (
               <Image
-                src={data.avatar}
-                width="100"
-                height="100"
-                loading="eager"
-                alt="Avatar"
-                className="rounded-full"
+                src={data.banner}
+                width="800"
+                height="300"
+                loading="lazy"
+                alt="Banner do perfil"
+                className="w-full rounded-tl rounded-tr border-b border-black/90"
               />
             )}
-            <h1 className="text-xl font-medium">{data.displayName}</h1>
-            <p className="max-w-sm border-l border-blue-600 pl-2">
-              {data.description}
-            </p>
-            <Score profile={data} />
-          </div>
-        </main>
+            <div className="flex -translate-y-1/4 flex-col gap-3 px-5 py-3">
+              {data.avatar && (
+                <Image
+                  src={data.avatar}
+                  width="100"
+                  height="100"
+                  loading="eager"
+                  alt="Avatar"
+                  className="rounded-full"
+                />
+              )}
+              <h1 className="text-xl font-medium">{data.displayName}</h1>
+              <p className="max-w-sm border-l border-blue-600 pl-2">
+                {data.description}
+              </p>
+              <Score profile={data} />
+            </div>
+          </main>
+        </div>
+  
+        <Footer />
       </div>
-
-      <Footer />
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="grid min-h-[100dvh] grid-rows-[1fr_auto] justify-items-center">
+          <h1>MEEEHH, bad handle {params.handle}</h1>
+      </div>
+    );
+  }
 }
 
 export const runtime = 'edge';
