d173ef71d85036c5fb40844b12f4492a75130f23
Your Name <caihebatista@gmail.com>
2024-09-14 19:27:54 -0300

    Feat: add the redirect handling

--- src/app/[handle]/input.tsx
+++ src/app/[handle]/input.tsx
@@ -1,32 +1,44 @@
 'use client';
 
 import { AtSign, MoveRight } from 'lucide-react';
-import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { FormEvent, useState } from 'react';
 
 export function Input() {
   const [blueskyHandle, setBlueskyHandle] = useState('');
+  const router = useRouter()
+  const handleForm = (event: FormEvent) => {
+    event.preventDefault();
+
+    if (!blueskyHandle.includes('.')) {
+      return router.push(`/${blueskyHandle}.bsky.social`);
+    }
+
+    return router.push(`/${blueskyHandle}`);
+  };
   return (
     <>
       <h3 className=" my-3">Quer analisar outra conta?</h3>
-      <div className="flex items-center gap-1 rounded-sm max-w-xl w-full">
+      <form onSubmit={handleForm} className="flex items-center gap-1 rounded-sm max-w-xl w-full">
         <div className="flex items-center gap-2 w-full rounded-bl rounded-tl bg-zinc-200 p-3">
           <AtSign className="text-blue-600" />
+            
           <input
-            type="text"
-            className="bg-zinc-200 outline-none"
-            placeholder="meunick.bsky.social"
-            onChange={(event) => setBlueskyHandle(event.target.value)}
-            value={blueskyHandle}
-            autoCapitalize="none"
+          type="text"
+          className="bg-zinc-200 outline-none"
+          placeholder="meunick.bsky.social"
+          onChange={(event) => setBlueskyHandle(event.target.value)}
+          value={blueskyHandle}
+          autoCapitalize="none"
           />
         </div>
         <button
           className="rounded-br rounded-tr bg-blue-600 p-3 text-white"
           aria-label="Analise"
-        >
+          >
           <MoveRight />
         </button>
-      </div>
+      </form>
     </>
   );
 }
